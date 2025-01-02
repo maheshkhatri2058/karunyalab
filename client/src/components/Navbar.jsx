f5acbd157f6f1b058155bb1cf17bba86c0324777
Mahesh <maheshkhatri2058@gmail.com>
2025-01-02 20:39:26 +0530

    updatedt

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
         <h2><i class="ri-phone-fill"></i>+91 9995154704/9747054704</h2>
        </div>
          <div className='flex gap-4'>
-          <a href='https://api.whatsapp.com/send?phone=919995154704&text=Hii!%20How%20can%20I%20help%20You?Karunya%20Medical%20lab' className='text-2xl'><i class="ri-whatsapp-fill"></i></a>
+          <a href='https://api.whatsapp.com/send?phone=919995154704' className='text-2xl'><i class="ri-whatsapp-fill"></i></a>
           <i className="ri-instagram-fill text-2xl"></i>
           <i className="ri-youtube-fill text-2xl"></i>
           <i className="ri-twitter-x-line text-2xl"></i>
@@ -36,7 +36,7 @@ const Navbar = () => {
         <Link to='/' className='hover:text-blue-600 py-2'>HOME</Link>
         <Link to='/contact' className='hover:text-blue-600 py-2'>CONTACT</Link>
         <a href='#services'  className='hover:text-blue-600 py-2'S>SERVICES</a>
-        <button className='btn btn-info text-white text-xl'><a href='https://api.whatsapp.com/send?phone=919995154704&text=Hii!%20How%20can%20I%20help%20You?Karunya%20Medical%20lab'>Book Appointment</a></button>
+        <button className='btn btn-info text-white text-xl'><a href='https://api.whatsapp.com/send?phone=919995154704'>Book Appointment</a></button>
 
 
 
